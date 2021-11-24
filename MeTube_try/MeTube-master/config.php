9679493282f2f04f6e521e41277b7da14b698178
Aditya Sahani <48151173+adityasahani@users.noreply.github.com>
2021-11-24 09:54:38 +0530

    Delete MeTube_try directory

--- MeTube_try/MeTube-master/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set('zend.ze1_compatibility_mode',0);
-$dbhost = "mysql1.cs.clemson.edu";
-$dbuser = "metube_pdks";
-$dbpass = "dbms1234";
-$database = "metube_3m9k";
-$db = mysqli_connect($dbhost, $dbuser, $dbpass, $database);
-?>
